170ff0ed41f0e7a8d358cea6dd6eee96669e5e7b
Tejas Mane <tsmane8787@gmail.com>
2024-06-08 15:13:36 +0530

    Footer Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,10 +9,10 @@ function Footer() {
       <div className="footer-container">
         <p className="footer-text">Tejas Mane <GiAstronautHelmet /></p>
         <div className="footer-links">
-          <a href="https://github.com/TejasMane" className="footer-link" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/iamtejas23" className="footer-link" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
-          <a href="https://www.linkedin.com/in/TejasMane" className="footer-link" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/iamtejas23" className="footer-link" target="_blank" rel="noopener noreferrer">
             LinkedIn
           </a>
         </div>
